a9e92d9d9ad3f5de5c274c07c6015edfa94b5e4c
Mayke Kend <maykekend@gmail.com>
2022-04-07 22:40:44 -0300

    Upload

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image_processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="mayke",
+    author_email="maykekend@gmail.com",
+    description="Descomplicando a criaçõa de pacotes de processamento de imagens em Python",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="my_github_repository_project_link",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
